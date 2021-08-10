ef18eb66c9bcfb853f88507067000f486044467c
adityasuryana <>
2021-08-10 19:36:00 +0700

    Adding feature or function

--- app/src/main/java/id/kotlin/inputnama/MainActivity.java
+++ app/src/main/java/id/kotlin/inputnama/MainActivity.java
@@ -3,12 +3,23 @@ package id.kotlin.inputnama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText TextNama;
+    TextView Hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextNama=(EditText)findViewById(R.id.InputNama);
+        Hasil=(TextView)findViewById(R.id.LblNama);
+    }
+    public void TampilNama(View v){
+        Hasil.setText("Kamu pasti, "+TextNama.getText()+" ya!");
     }
 }
